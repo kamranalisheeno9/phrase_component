c5f23ea1c27cd051b80cf29a6d557c8353cf66ef
kamranalisheeno9 <kamranalisheeno9@gmail.com>
2022-12-22 02:39:39 +0500

    all changes done

--- src/components/Phrase.jsx
+++ src/components/Phrase.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
-import rightImg from '../assets/right.webp'
-import wrongImg from '../assets/wrong.webp'
+import rightImg from "../assets/right.webp";
+import wrongImg from "../assets/wrong.webp";
+import wrongSound from "../assets/wrongaction.wav";
+import rightSound from "../assets/activitycompletepluck.wav";
 import "./Phrase.css";
 const Phrase = () => {
+  // Phrases Data
+
   const phrases = [
     {
       text: "Live with gratitude",
@@ -35,18 +39,58 @@ const Phrase = () => {
       right: "be humble to people",
     },
   ];
+
+  // State Variaable Declared
+
   const [repeatedPhrases, setRepeatedPhrases] = useState([]);
   const [num, setNum] = useState(0);
   const [active, setActive] = useState(false);
   const [active2, setActive2] = useState(false);
   const [points, setPoints] = useState(0);
-  const newRandomNumber = () => {
+  const [mins, setMins] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [finalResult, setFinalResult] = useState(false);
+
+  // Timer Function
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      setSeconds(seconds + 1);
+    }, 1000);
+    if (mins === 4) {
+      clearTimeout(timeoutID);
+      setFinalResult(true);
+    } else if (seconds === 60) {
+      setSeconds(0);
+      setMins(mins + 1);
+    }
+  });
+
+  // Random Data Occuring Function
+
+  function newRandomNumber() {
     const newNum = Math.floor(Math.random() * phrases.length);
     setNum(newNum);
+  }
+  useEffect(() => {
+    newRandomNumber();
+  }, []);
+
+  // Sound Play Functions
+
+  const wrongPlay = () => {
+    new Audio(wrongSound).play();
   };
+  const rightPlay = () => {
+    new Audio(rightSound).play();
+  };
+
+  // Right Answer Function
+
   const addPoints = (correct) => {
+    rightPlay();
     if (correct === phrases[num].text) {
-      setPoints(points + 5);
+      setPoints(points + 1);
 
       repeatedPhrases.map((repeated) => {
         if (repeated.text === phrases[num].text) {
@@ -71,7 +115,11 @@ const Phrase = () => {
       setActive(false);
     }, 2000);
   };
-  const Wrong = (incorrect) => {
+
+  // Wrong Answer Function
+
+  const Wrong = () => {
+    wrongPlay();
     setActive2(true);
     setTimeout(() => {
       newRandomNumber();
@@ -81,60 +129,61 @@ const Phrase = () => {
       setActive2(false);
     }, 2000);
   };
-  useEffect(() => {
-    newRandomNumber();
-  }, []);
+
   return (
-    <div>
-      <div className="points">
-        <span>Points : </span> {points}
-      </div>
-      <div className="card_done_container">
-        <div className={active ? "active done" : "inactive done"}>
-          {active ? (
-            <img
-              width="50px"
-              src={rightImg}
-              alt="right"
-            />
-          ) : (
-            <></>
-          )}
+    <>
+      {finalResult ? (
+        // Showing Final Result Component
+
+        <div className="final_card_container">
+          <p> Final Result : {points}</p>
         </div>
-        <div className={active2 ? "active done" : "inactive done"}>
-          {active2 ? (
-            <img
-              width="50px"
-              src={wrongImg}
-              alt="wrong"
-            />
-          ) : (
-            <></>
-          )}
+      ) : (
+        // Main Phrase Component
+
+        <div>
+          <div className="points">
+            <div>
+              {mins} : {seconds}
+            </div>
+            <span> Ambients : </span> {points} {points <= 1 ? "pt" : "pts"}
+          </div>
+          <div className="card_done_container">
+            <div className={active ? "active done" : "inactive done"}>
+              {active ? <img width="50px" src={rightImg} alt="right" /> : <></>}
+            </div>
+            <div className={active2 ? "active done" : "inactive done"}>
+              {active2 ? (
+                <img width="50px" src={wrongImg} alt="wrong" />
+              ) : (
+                <></>
+              )}
+            </div>
+            <div
+              className={
+                active2 || active
+                  ? "card-container activeCard"
+                  : "card-container inactiveCard"
+              }
+            >
+              <p className="phrase">{phrases[num].text}</p>
+            </div>
+          </div>
+          <div className="buttons">
+            <div className="wrong" onClick={active2 ? null : () => Wrong()}>
+              <div className="circle-btn"> X</div>
+            </div>
+            <p className="or">or</p>
+            <div
+              className="right"
+              onClick={active ? null : () => addPoints(phrases[num].right)}
+            >
+              <div className="circle-btn"> &#10004;</div>
+            </div>
+          </div>
         </div>
-        <div
-          className={
-            active2 || active
-              ? "card-container activeCard"
-              : "card-container inactiveCard"
-          }
-        >
-          <p className="phrase">{phrases[num].text}</p>
-        </div>
-      </div>
-      <div className="buttons">
-        <div className="wrong" onClick={active2 ? null : () => Wrong()}>
-          <div className="circle-btn"> X</div>
-        </div>
-        <p className="or">or</p>
-        <div
-          className="right"
-          onClick={active ? null : () => addPoints(phrases[num].right)}
-        >
-          <div className="circle-btn"> &#10004;</div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
